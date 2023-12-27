2504990b0d02b57c240d38f314db460f101620c8
陳鈞 <jim60105@gmail.com>
2023-12-28 06:03:18 +0800

    feat: add handling old channel IDs for twitcasting
    - Add conditionally compiled code for different database options
    - Move `LivestreamRecorder.DB.Interfaces` namespace to the top
    - Add a temporary workaround for handling old channel IDs
    - Update channel IDs in the database with a new format
    - Commit changes to the database after updating channel IDs
    - Add a comment describing the `HandleOldChannelIdAsync` method
    
    Signed-off-by: 陳鈞 <jim60105@gmail.com>

--- Workers/UpdateChannelInfoWorker.cs
+++ Workers/UpdateChannelInfoWorker.cs
@@ -1,5 +1,14 @@
-﻿using LivestreamRecorderService.Interfaces;
+﻿#if COSMOSDB
+using LivestreamRecorder.DB.CosmosDB;
+#elif COUCHDB
+using LivestreamRecorder.DB.CouchDB;
+#endif
+using LivestreamRecorder.DB.Interfaces;
+using LivestreamRecorder.DB.Models;
+using LivestreamRecorderService.Helper;
+using LivestreamRecorderService.Interfaces;
 using LivestreamRecorderService.ScopedServices.PlatformService;
+using Omu.ValueInjecter;
 using Serilog.Context;
 
 namespace LivestreamRecorderService.Workers;
@@ -11,6 +20,8 @@ public class UpdateChannelInfoWorker(
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         using var _ = LogContext.PushProperty("Worker", nameof(UpdateChannelInfoWorker));
+
+        await HandleOldChannelIdAsync();
 
 #if RELEASE
         logger.LogInformation("{Worker} will sleep 60 seconds avoid being overloaded with {WorkerToWait}.", nameof(RecordWorker), nameof(MonitorWorker));
@@ -35,6 +46,43 @@ public class UpdateChannelInfoWorker(
             logger.LogTrace("{Worker} ends. Sleep 1 day.", nameof(UpdateChannelInfoWorker));
             await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
         }
+    }
+
+    /// <summary>
+    /// Handle old channel id. This is a temporary workaround for the channel id changes.
+    /// </summary>
+    /// <returns></returns>
+    private async Task HandleOldChannelIdAsync()
+    {
+        logger.LogInformation("Start to handle old channel id.");
+        using var scope = serviceProvider.CreateScope();
+        var channelRepository = scope.ServiceProvider.GetRequiredService<IChannelRepository>();
+#pragma warning disable CA1859 // 盡可能使用具象類型以提高效能
+        IUnitOfWork unitOfWork = scope.ServiceProvider.GetRequiredService<UnitOfWork_Public>();
+#pragma warning restore CA1859 // 盡可能使用具象類型以提高效能
+
+        var channels = await channelRepository.GetChannelsBySourceAsync("Twitcasting");
+        if (channels.Count == 0)
+        {
+            logger.LogInformation("No old channel id needs to update.");
+            return;
+        }
+
+        int count = 0;
+        foreach (var channel in channels)
+        {
+            if (!channel.id.StartsWith('T'))
+            {
+                var newChannel = Mapper.Map<Channel>(channel);
+                newChannel.id = NameHelper.ChangeId.ChannelId.DatabaseType(channel.id, "Twitcasting");
+                await channelRepository.DeleteAsync(channel);
+                await channelRepository.AddOrUpdateAsync(newChannel);
+                count++;
+            }
+        }
+        unitOfWork.Commit();
+
+        logger.LogInformation("Handled {count} old channel id.", count);
     }
 
     private async Task UpdatePlatformAsync(IPlatformService platformService, CancellationToken stoppingToken = default)
